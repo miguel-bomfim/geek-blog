0a5900b9093f98556ed0682da89b1bd3aaf70294
Miguel Bomfim <60234561+miguel-bomfim@users.noreply.github.com>
2025-01-14 23:01:15 -0300

    Update movielist.tsx

--- components/movielist.tsx
+++ components/movielist.tsx
@@ -1,6 +1,12 @@
-import { Movie } from "../types";
 //const imageSize = "w500";
 // const tmdbImagesURL = `https://image.tmdb.org/t/p/${imageSize}`;
+
+type Movie = {
+  title: string
+  releaseDate: string
+  poster: string
+  review: string
+}
 
 export default function MovieList({ movies }: { movies: Movie[] }) {
   return <p className="text-black hidden">{movies[0].title}</p>;
